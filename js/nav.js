59ac454e2bc9888978982f8f8e748a3888625eff
bluishfrog <frogbluish@gmail.com>
2026-03-26 21:50:16 +0100

    nav stuff

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,14 @@
 function toggleMenu(button) {
     const nav = button.closest('.nav-content');
+    event.stopPropagation();
     nav.classList.toggle('menu-open');
 }
+
+document.addEventListener('click', function (event) {
+    const nav = document.querySelector('.nav-content');
+    const isClickInside = nav.contains(event.target);
+
+    if (!isClickInside) {
+        nav.classList.remove('menu-open');
+    }
+});
